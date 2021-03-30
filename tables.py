7620012cc42e9b9609d9ed8726711b83bc6e2376
Arsa Izdihar Islam <arsadihar@gmail.com>
2021-03-30 16:49:15 +0700

    delete line and twitter bot

--- tables.py
+++ tables.py
@@ -149,20 +149,3 @@ class File(db.Model):
 
     def __str__(self):
         return self.filename
-
-
-class LineAccount(db.Model):
-    account_id = db.Column(db.String(50), primary_key=True)
-    name = db.Column(db.String(20))
-    last_tweet = db.Column(db.String(100))
-    img_soon = db.Column(db.Boolean, default=False)
-    last_tweet_req = db.Column(db.String(100))
-    next_tweet_msg = db.Column(db.Text)
-    tweet_phase = db.Column(db.String(50))
-
-
-class LineGroup(db.Model):
-    id = db.Column(db.String(50), primary_key=True)
-    phase = db.Column(db.String(50))
-    data = db.Column(db.Text)
-    member_ids = db.Column(db.Text)
